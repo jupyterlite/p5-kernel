daed87052a6076346dc4ba0e9cecdc095eac714e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-29 12:15:10 +0200

    Fix labextension hook

--- p5_kernel_lite/__init__.py
+++ p5_kernel_lite/__init__.py
@@ -1,2 +1,15 @@
+import json
+from pathlib import Path
 
 from ._version import __version__
+
+HERE = Path(__file__).parent.resolve()
+
+with (HERE / "liteextension" / "package.json").open() as fid:
+    data = json.load(fid)
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "liteextension",
+        "dest": data["name"]
+    }]
